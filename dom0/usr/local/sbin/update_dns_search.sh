3e31b1ad26b150fb4ac8f966d7785de51283e09f
Heiko Baumann <baumann@hs-koblenz.de>
2020-07-23 16:29:46 +0200

    Updated script to work on qubes 4.0

--- dom0/usr/local/sbin/update_dns_search.sh
+++ dom0/usr/local/sbin/update_dns_search.sh
@@ -1,39 +1,41 @@
 #!/bin/bash
-CHECK_VMS="$QREXEC_REMOTE_DOMAIN"
+QREXEC_REMOTE_DOMAIN="sys-net"
+#CHECK_VMS="$QREXEC_REMOTE_DOMAIN"
+CHECK_VMS="personal office"
 VM_UPDATE_SCRIPT="/usr/sbin/update_dns_search.sh"
 DNS_SEARCH="$(qubesdb-read -d "$QREXEC_REMOTE_DOMAIN" "/qubes-netvm-dns-search")"
 
-get_slave_vms () {
-	qvm-ls --raw-data name netvm | tr -d '{}[]*=>' | grep "|$1" | cut -d '|' -f 1 | tr '\n' ' '
+get_net_vm () {
+	local VM="$1"
+	local X_VM="$VM"
+	local Y_VM="$VM"
+	while true ; do
+		X_VM="$(qvm-ls --raw-data --fields name,netvm "$Y_VM" 2> /dev/null | cut -d '|' -f 2)"
+		if [ "$X_VM" = "-" ] ; then
+			echo "$Y_VM"
+			break
+		fi
+		Y_VM="$X_VM"
+	done
 }
 
 vm_is_running () {
 	qvm-check --running "$1" > /dev/null 2>&1
 }
 
-ALL_SLAVE_VMS=""
-while true ; do
-	SLAVE_VMS=""
-	for VM in $CHECK_VMS ; do
-		X_SLAVE_VMS="$(get_slave_vms $VM)"
-		if [ "$X_SLAVE_VMS" = "" ] ; then
-			continue
-		fi
-		SLAVE_VMS="$SLAVE_VMS $X_SLAVE_VMS"
-	done
-	CHECK_VMS="$SLAVE_VMS"
-	if [ "$CHECK_VMS" = "" ] ; then
-		break
+MODIFY_VMS=""
+for VM in $CHECK_VMS ; do
+	NET_VM="$(get_net_vm "$VM")"
+	if [ "$NET_VM" != "$QREXEC_REMOTE_DOMAIN" ] ; then
+		continue
 	fi
-	for VM in $SLAVE_VMS ; do
-		if ! vm_is_running "$VM" ; then
-			continue
-		fi
-		ALL_SLAVE_VMS="$ALL_SLAVE_VMS $VM"
-	done
+	MODIFY_VMS="$MODIFY_VMS $VM"
 done
 
-for VM in $ALL_SLAVE_VMS ; do
+for VM in $MODIFY_VMS ; do
+	if ! vm_is_running "$VM" ; then
+		continue
+	fi
 	echo "Updating DNS search for VM $VM..."
 	qubesdb-write -d "$VM" "/qubes-dns-search" "$DNS_SEARCH"
 	qvm-run "$VM" "sudo $VM_UPDATE_SCRIPT"
